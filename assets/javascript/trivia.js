5da40e9697a6fb2b99c5dbb00cc2d640667a6ae7
Edward Hopper <burson.kate@gmail.com>
2019-01-16 21:42:23 -0800

    html, some css, initial js

---
+++ assets/javascript/trivia.js
@@ -0,0 +1,16 @@
+var questions = [];
+var answers = [];
+var correctAnswers = [];
+
+var currentQuestion = [];
+var currentAnswer = [];
+
+var correct = 0;
+var incorrect = 0;
+var started = false;
+
+function question() {
+    for(var i = 0; i < questions.length; i++) {
+        
+    }
+}
